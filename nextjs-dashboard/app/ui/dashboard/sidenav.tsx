c655c21a8c4192879fe5019e089fe93644442bc9
xuyuanjie <971260057@qq.com>
2024-08-02 14:15:53 +0800

    feat: login-new

--- nextjs-dashboard/app/ui/dashboard/sidenav.tsx
+++ nextjs-dashboard/app/ui/dashboard/sidenav.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
-import { signOut } from '../../auth';
+import { signOut } from '../../../auth';
 
 export const experimental_ppr = true;
 
